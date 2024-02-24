21d85fa138b2ba23f7ac8578e39743d2905f34f4
madrigueira <mndias29@gmail.com>
2024-02-24 03:00:03 -0300

    coloquei barra de pesquisa (auuuuuuu late coração cachorro late coração

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
                 title
                 slug
                 updatedAt
-                series{
+                series(first:100){
                   title
                   slug
                   updatedAt
--- src/pages/Comics/index.jsx
+++ src/pages/Comics/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.scss"
 import { Link } from 'react-router-dom';
+import { IoIosSearch } from "react-icons/io"; 
 import ComicCover from '../../components/ComicCover';
 
 const Comics = ({ comic }) => {
@@ -19,14 +20,36 @@ const Comics = ({ comic }) => {
     `,
   };
 
+  const [search, setSearch] = useState("")
+  let serieTitle = comic.series.map((serie) => serie)
+  let newSerie = []
+  let n = 0
+  if(search != undefined){
+    for (let i = 0; i < serieTitle.length; i++) {
+      if(serieTitle[i].title.toLowerCase().includes(search)){
+        newSerie[n] = serieTitle[i]
+        n++
+      }
+    } 
+  }
+  
+  const [searchBarState, setSearchBarState] = useState(false)
+  const toggleSearchBar = () => {
+    setSearchBarState(!searchBarState)
+  }
+
   return (
     <div className='Comics'>
       <div className="container">
         <div className="bg" style={bg}></div>
+        <div className="search">
+          <button className={searchBarState ? 'active' : ''} onClick={toggleSearchBar}><IoIosSearch /></button>
+          <input type="text" className={searchBarState ? 'active' : ''} placeholder='Pesquisar...' onChange={(event) => {setSearch(event.target.value.toLowerCase())}} />
+        </div>
         <div className="box">
-          {comic.series.map((serie) => (
+          {newSerie.map((serie) => (
             <Link key={serie.slug} to={`${mainUrl}/${urlPublisher}/${urlComic}/${serie.slug}`}>
-              <ComicCover key={serie.slug} slug={serie.slug} title={serie.title} urlPublisher={urlPublisher} urlComic={urlComic}/>
+              <ComicCover id={serie.title} key={serie.slug} slug={serie.slug} title={serie.title} urlPublisher={urlPublisher} urlComic={urlComic}/>
             </Link>
           ))}
         </div>
